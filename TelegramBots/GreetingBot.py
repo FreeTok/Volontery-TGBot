e3059af3b8893f0c2adbb2ab9458e5f9e55ae103
FreeTok <freetokdevelop@gmail.com>
2022-11-27 22:02:43 +0300

    Remove directions numbers

--- TelegramBots/GreetingBot.py
+++ TelegramBots/GreetingBot.py
@@ -55,75 +55,28 @@ def lalala(message):
             bot.send_message(message.chat.id, f'👨‍👩‍👧Волонтеры Победы - это не просто общественное движение, а целая жизнь и семья. По всей стране и даже за рубежом налаживается связь поколений между молодежью и пожилыми людьми, большое количество молодых людей вовлекается в волонтерскую деятельность. За 7 лет в Нижегородской области было проведено более 4500 мероприятий разных тематик инаправлений. \nХочешь узнать немного подробнее?)')
 
         elif message.text == 'Наши направления':
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item1 = types.InlineKeyboardButton("1")
-            item2 = types.InlineKeyboardButton("2")
-            item3 = types.InlineKeyboardButton("3")
-            item4 = types.InlineKeyboardButton("4")
-            item5 = types.InlineKeyboardButton("5")
-            item6 = types.InlineKeyboardButton("6")
+            markup = types.InlineKeyboardMarkup(row_width=1)
+            item1 = types.InlineKeyboardButton("Великая Победа", callback_data='1')
+            item2 = types.InlineKeyboardButton("Связь Поколений", callback_data='2')
+            item3 = types.InlineKeyboardButton("Моя Победа", callback_data='3')
+            item4 = types.InlineKeyboardButton("Наши Победы", callback_data='4')
+            item5 = types.InlineKeyboardButton("Медиа Победа", callback_data='5')
+            item6 = types.InlineKeyboardButton("Моя история", callback_data='6')
 
             markup.add(item1, item2, item3, item4, item5, item6)
 
-            bot.send_message(message.chat.id, 'Направления Движения: \n1) Великая Победа -  сопровождение Парадов Победы и Бессмертного Полка, почитание памяти героев ВОВ и др. \n2) Связь Поколений - помощь ветеранам ВОВ, акция "Красная Гвоздика" и др. \n3) Моя Победа - обучение новых волонтеров по интересной программе, приложение Skill cup и др.  \n4) Наши Победы - рассказ о великих подвигах в интересных форматах (Квесты, игры, квизы) \n5) Медиа Победа - Медиа пространство, SMM -  специалисты и тд. \n6) Моя история - составление семейного древа, работа с архивом.', reply_markup=markup)
+            bot.send_message(message.chat.id, 'Направления Движения: \n'
+                                              '\1) Великая Победа -  сопровождение Парадов Победы и Бессмертного Полка, почитание памяти героев ВОВ и др. '
+                                              '\n2) Связь Поколений - помощь ветеранам ВОВ, акция "Красная Гвоздика" и др. '
+                                              '\n3) Моя Победа - обучение новых волонтеров по интересной программе, приложение Skill cup и др.  '
+                                              '\n4) Наши Победы - рассказ о великих подвигах в интересных форматах (Квесты, игры, квизы) '
+                                              '\n5) Медиа Победа - Медиа пространство, SMM -  специалисты и тд. '
+                                              '\n6) Моя история - составление семейного древа, работа с архивом.', reply_markup=markup)
 
 
         elif message.text == 'Почему мы?':
             bot.send_message(message.chat.id, 'С нами ты можешь получить:\n- Баллы при поступление в учебный заведения; \n- Есть возможность ездить на познавательные форумы в разные города; \n- Участвовать в конкурсах; \n- Создавать свои проекты \n- Получить классную атрибутику \n- Стать частью большой, дружной семьи \n- И море классных воспоминаний, опыта и позитивных эмоций; \n- А для участников(!)нашего движения младше 18 лет есть уникальная возможность попасть в топовые лагеря СТРАНЫ! 🇷🇺)')
 
-        elif message.text == '1':
-            markup = types.InlineKeyboardMarkup(row_width=1)
-            item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
-            markup.add(item7)
-            bot.send_message(message.chat.id, '«Великая Победа» \nВ рамках направления «Великая Победа» добровольцы организовывают волонтёрское сопровождение Парадов Победы и Бессмертного Полка в городах России, благоустраивают памятные места и воинские захоронения, проводят различные всероссийские акции и проекты, такие как «Георгиевская ленточка», «Свеча памяти», «Внуки Победы» и другие.')
-            bot.send_message(message.chat.id, '[Смотреть видео](https://drive.google.com/file/d/17WzrzDzW9NuiRVvhZY0QoBPDBc4eBL3K/view?usp=share_link)', parse_mode='Markdown' , reply_markup=markup)
-            # bot.send_video(message.chat.id, video)
-
-        elif message.text == '2':
-            markup = types.InlineKeyboardMarkup(row_width=1)
-            item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
-            markup.add(item7)
-            bot.send_message(message.chat.id, '«Связь поколений» \nДобровольцы направления «Связь поколений» проводят Всероссийскую акцию «Красная гвоздика» совместно с Благотворительным фондом «Память Поколений», помогая не только словом, но и реальным делом. Все вырученные с акции средства направляются на высокотехнологичную медицинскую помощь ветеранам.')
-            bot.send_message(message.chat.id, '[Смотреть видео](https://drive.google.com/file/d/1OQ-uvhJCsr3S3jZ0kcO5SMCdcpsm4TWc/view?usp=share_link)', parse_mode='Markdown', reply_markup=markup)
-
-        elif message.text == '3':
-            markup = types.InlineKeyboardMarkup(row_width=1)
-            item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
-            markup.add(item7)
-            bot.send_message(message.chat.id, '«Моя победа» \nВ рамках этого направления доброволец учится создавать команду и управлять ею, разрабатывать и реализовывать крупные проекты, правильно использовать ресурсы и многому другому.')
-            bot.send_message(message.chat.id,
-                             '[Смотреть видео](https://drive.google.com/file/d/1RnFGvUP2wgBQwTk_kuVfPCWtlUlgVaVX/view?usp=share_link)',
-                             parse_mode='Markdown', reply_markup=markup)
-
-
-        elif message.text == '4':
-            markup = types.InlineKeyboardMarkup(row_width=1)
-            item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
-            markup.add(item7)
-            bot.send_message(message.chat.id,'«Наши победы» \nМы рассказываем о Героях и достижениях страны через интеллектуальные игры, городские квесты, всероссийские акции.')
-            bot.send_message(message.chat.id,
-                             '[Смотреть видео](https://drive.google.com/file/d/18OBtt_Uki_Zv3pTGLO_b9zUsVAeFnpvW/view?usp=share_link)',
-                             parse_mode='Markdown', reply_markup=markup)
-
-        elif message.text == '5':
-            markup = types.InlineKeyboardMarkup(row_width=1)
-            item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
-            markup.add(item7)
-            bot.send_message(message.chat.id, '«Медиапобеда» \nВолонтёры направления «Медиапобеда» занимаются производством позитивного контента: пишут статьи, снимают фото и видео, ведут группы и аккаунты в социальных сетях. Активисты создают медиапоток, несущий в себе исторические, интересные и познавательные инфоповоды.')
-            video = open('video5.mp4', 'rb')
-            bot.send_message(message.chat.id,
-                             '[Смотреть видео](https://drive.google.com/file/d/1gssQR1vCcktO44fmkEdrt-KsxZP2OPxG/view?usp=share_link)',
-                             parse_mode='Markdown', reply_markup=markup)
-
-
-        elif message.text == '6':
-            markup = types.InlineKeyboardMarkup(row_width=1)
-            item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
-            markup.add(item7)
-            bot.send_message(message.chat.id, '«Моя история» \n«Моя история» — это первый в России проект, который не просто говорит о важности изучения истории семьи, но и даёт конкретный механизм, как это сделать с помощью волонтёров. Уже с весны 2019 года любой житель нашей страны может воспользоваться помощью добровольцев в составлении семейного древа.')
-            bot.send_message(message.chat.id,
-                             '[Смотреть видео](https://drive.google.com/file/d/17AhdJBdDknrkE27ZjCYl-fgCzkn0bBIq/view?usp=share_link)',
-                             parse_mode='Markdown', reply_markup=markup)
 
         elif message.text == 'Назад':
 
@@ -152,13 +105,13 @@ def callback_inline(call):
                                  f'👨‍👩‍👧Волонтеры Победы - это не просто общественное движение, а целая жизнь и семья. По всей стране и даже за рубежом налаживается связь поколений между молодежью и пожилыми людьми, большое количество молодых людей вовлекается в волонтерскую деятельность. За 7 лет в Нижегородской области было проведено более 4500 мероприятий разных тематик инаправлений. \nХочешь узнать немного подробнее - смотри направления)', reply_markup=markup)
 
             elif call.data == "Directions":
-                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-                item1 = types.InlineKeyboardButton("1")
-                item2 = types.InlineKeyboardButton("2")
-                item3 = types.InlineKeyboardButton("3")
-                item4 = types.InlineKeyboardButton("4")
-                item5 = types.InlineKeyboardButton("5")
-                item6 = types.InlineKeyboardButton("6")
+                markup = types.InlineKeyboardMarkup(row_width=1)
+                item1 = types.InlineKeyboardButton("Великая Победа", callback_data='1')
+                item2 = types.InlineKeyboardButton("Связь Поколений", callback_data='2')
+                item3 = types.InlineKeyboardButton("Моя Победа", callback_data='3')
+                item4 = types.InlineKeyboardButton("Наши Победы", callback_data='4')
+                item5 = types.InlineKeyboardButton("Медиа Победа", callback_data='5')
+                item6 = types.InlineKeyboardButton("Моя история", callback_data='6')
 
 
                 markup.add(item1, item2, item3, item4, item5, item6)
@@ -206,13 +159,13 @@ def callback_inline(call):
                 bot.send_message(call.message.chat.id, 'Что ты хочешь узнать о нашем движении?', reply_markup=markup)
 
             elif call.data == 'Back2':
-                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-                item1 = types.InlineKeyboardButton("1")
-                item2 = types.InlineKeyboardButton("2")
-                item3 = types.InlineKeyboardButton("3")
-                item4 = types.InlineKeyboardButton("4")
-                item5 = types.InlineKeyboardButton("5")
-                item6 = types.InlineKeyboardButton("6")
+                markup = types.InlineKeyboardMarkup(row_width=1)
+                item1 = types.InlineKeyboardButton("Великая Победа", callback_data='1')
+                item2 = types.InlineKeyboardButton("Связь Поколений", callback_data='2')
+                item3 = types.InlineKeyboardButton("Моя Победа", callback_data='3')
+                item4 = types.InlineKeyboardButton("Наши Победы", callback_data='4')
+                item5 = types.InlineKeyboardButton("Медиа Победа", callback_data='5')
+                item6 = types.InlineKeyboardButton("Моя история", callback_data='6')
 
                 markup.add(item1, item2, item3, item4, item5, item6)
 
@@ -233,6 +186,70 @@ def callback_inline(call):
                     config.reg.append(call.message.chat.id)
                     print(len(config.reg))
                     bot.send_message(call.message.chat.id, '[Стать волонтёром](https://xn--90abhd2amfbbjkx2jf6f.xn--p1ai/)', parse_mode='Markdown')
+
+            elif call.data == '1':
+                markup = types.InlineKeyboardMarkup(row_width=1)
+                item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
+                markup.add(item7)
+                bot.send_message(call.message.chat.id,
+                                 '«Великая Победа» \nВ рамках направления «Великая Победа» добровольцы организовывают волонтёрское сопровождение Парадов Победы и Бессмертного Полка в городах России, благоустраивают памятные места и воинские захоронения, проводят различные всероссийские акции и проекты, такие как «Георгиевская ленточка», «Свеча памяти», «Внуки Победы» и другие.')
+                bot.send_message(call.message.chat.id,
+                                 '[Смотреть видео](https://drive.google.com/file/d/17WzrzDzW9NuiRVvhZY0QoBPDBc4eBL3K/view?usp=share_link)',
+                                 parse_mode='Markdown', reply_markup=markup)
+                # bot.send_video(message.chat.id, video)
+
+            elif call.data == '2':
+                markup = types.InlineKeyboardMarkup(row_width=1)
+                item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
+                markup.add(item7)
+                bot.send_message(call.message.chat.id,
+                                 '«Связь поколений» \nДобровольцы направления «Связь поколений» проводят Всероссийскую акцию «Красная гвоздика» совместно с Благотворительным фондом «Память Поколений», помогая не только словом, но и реальным делом. Все вырученные с акции средства направляются на высокотехнологичную медицинскую помощь ветеранам.')
+                bot.send_message(call.message.chat.id,
+                                 '[Смотреть видео](https://drive.google.com/file/d/1OQ-uvhJCsr3S3jZ0kcO5SMCdcpsm4TWc/view?usp=share_link)',
+                                 parse_mode='Markdown', reply_markup=markup)
+
+            elif call.data == '3':
+                markup = types.InlineKeyboardMarkup(row_width=1)
+                item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
+                markup.add(item7)
+                bot.send_message(call.message.chat.id,
+                                 '«Моя победа» \nВ рамках этого направления доброволец учится создавать команду и управлять ею, разрабатывать и реализовывать крупные проекты, правильно использовать ресурсы и многому другому.')
+                bot.send_message(call.message.chat.id,
+                                 '[Смотреть видео](https://drive.google.com/file/d/1RnFGvUP2wgBQwTk_kuVfPCWtlUlgVaVX/view?usp=share_link)',
+                                 parse_mode='Markdown', reply_markup=markup)
+
+
+            elif call.data == '4':
+                markup = types.InlineKeyboardMarkup(row_width=1)
+                item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
+                markup.add(item7)
+                bot.send_message(call.message.chat.id,
+                                 '«Наши победы» \nМы рассказываем о Героях и достижениях страны через интеллектуальные игры, городские квесты, всероссийские акции.')
+                bot.send_message(call.message.chat.id,
+                                 '[Смотреть видео](https://drive.google.com/file/d/18OBtt_Uki_Zv3pTGLO_b9zUsVAeFnpvW/view?usp=share_link)',
+                                 parse_mode='Markdown', reply_markup=markup)
+
+            elif call.data == '5':
+                markup = types.InlineKeyboardMarkup(row_width=1)
+                item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
+                markup.add(item7)
+                bot.send_message(call.message.chat.id,
+                                 '«Медиапобеда» \nВолонтёры направления «Медиапобеда» занимаются производством позитивного контента: пишут статьи, снимают фото и видео, ведут группы и аккаунты в социальных сетях. Активисты создают медиапоток, несущий в себе исторические, интересные и познавательные инфоповоды.')
+                video = open('video5.mp4', 'rb')
+                bot.send_message(call.message.chat.id,
+                                 '[Смотреть видео](https://drive.google.com/file/d/1gssQR1vCcktO44fmkEdrt-KsxZP2OPxG/view?usp=share_link)',
+                                 parse_mode='Markdown', reply_markup=markup)
+
+
+            elif call.data == '6':
+                markup = types.InlineKeyboardMarkup(row_width=1)
+                item7 = types.InlineKeyboardButton('Назад', callback_data='Back2')
+                markup.add(item7)
+                bot.send_message(call.message.chat.id,
+                                 '«Моя история» \n«Моя история» — это первый в России проект, который не просто говорит о важности изучения истории семьи, но и даёт конкретный механизм, как это сделать с помощью волонтёров. Уже с весны 2019 года любой житель нашей страны может воспользоваться помощью добровольцев в составлении семейного древа.')
+                bot.send_message(call.message.chat.id,
+                                 '[Смотреть видео](https://drive.google.com/file/d/17AhdJBdDknrkE27ZjCYl-fgCzkn0bBIq/view?usp=share_link)',
+                                 parse_mode='Markdown', reply_markup=markup)
 
 
 
